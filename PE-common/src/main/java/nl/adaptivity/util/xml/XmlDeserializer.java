a269ebb8748b1f40c17eda223ed5cf95ccc255ce
Paul de Vrieze <paul@devrieze.net>
2015-08-27 15:53:30 +0100

    make resultholders deserialize through pre-exising jaxb to xmlserializable mapping. This should work better with namespace info.

--- PE-common/src/main/java/nl/adaptivity/util/xml/XmlDeserializer.java
+++ PE-common/src/main/java/nl/adaptivity/util/xml/XmlDeserializer.java
@@ -1,8 +1,16 @@
 package nl.adaptivity.util.xml;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
 /**
  * Created by pdvrieze on 27/08/15.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface XmlDeserializer {
 
   Class<? extends XmlDeserializerFactory> value();
--- PE-common/src/main/java/nl/adaptivity/util/xml/XmlSerializable.java
+++ PE-common/src/main/java/nl/adaptivity/util/xml/XmlSerializable.java
@@ -3,29 +3,41 @@ package nl.adaptivity.util.xml;
 import net.devrieze.annotations.NotNull;
 import org.w3c.dom.*;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.stream.*;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public interface XmlSerializable {
 
   @XmlAccessorType(XmlAccessType.PROPERTY)
   public static class SimpleAdapter {
+    private volatile static MethodHandle _getContext;
+    private volatile static boolean _failedReflection = false;
+    private static MethodHandle _getAllDeclaredPrefixes;
+    private static MethodHandle _getNamespaceURI;
 
     QName name;
+    private SimpleNamespaceContext namespaceContext;
 
     public Map<QName, Object> getAttributes() {
       return attributes;
@@ -51,20 +63,55 @@ public interface XmlSerializable {
 
     public void beforeUnmarshal(Unmarshaller unmarshaller, Object parent) {
       if (parent instanceof JAXBElement) {
-        name=((JAXBElement) parent).getName();
+        name = ((JAXBElement) parent).getName();
+      }
+
+      if (_failedReflection) {
+        return;
+      }
+      Object context;
+      try {
+        if (_getContext == null) {
+          synchronized (getClass()) {
+            Lookup lookup = MethodHandles.lookup();
+            _getContext = lookup.unreflect(unmarshaller.getClass().getMethod("getContext"));
+            context = _getContext.invoke(unmarshaller);
+            _getAllDeclaredPrefixes = lookup.unreflect(context.getClass().getMethod("getAllDeclaredPrefixes"));
+            _getNamespaceURI = lookup.unreflect(context.getClass().getMethod("getNamespaceURI", String.class));
+
+          }
+        } else {
+          context = _getContext.invoke(unmarshaller);
+        }
+
+        if (context != null) {
+          String[] prefixes = (String[]) _getAllDeclaredPrefixes.invoke(context);
+          if (prefixes != null && prefixes.length > 0) {
+            String[] namespaces = new String[prefixes.length];
+            for (int i = prefixes.length - 1; i >= 0; --i) {
+              namespaces[i] = (String) _getNamespaceURI.invoke(context, prefixes[i]);
+            }
+            namespaceContext = new SimpleNamespaceContext(prefixes, namespaces);
+          }
+        }
+
+      } catch (Throwable e) {
+        Logger.getAnonymousLogger().log(Level.FINE, "Could not retrieve namespace context from marshaller", e);
+        _failedReflection = true;
       }
     }
+
   }
 
-  public static class JAXBUnmarshallingAdapter extends JAXBAdapter {
+  public static class JAXBUnmarshallingAdapter<T extends XmlSerializable> extends JAXBAdapter {
 
     @NotNull
-    private final XmlDeserializerFactory<?> mFactory;
+    private final XmlDeserializerFactory<T> mFactory;
 
-    public JAXBUnmarshallingAdapter(Class<? extends XmlSerializable> targetType) {
+    public JAXBUnmarshallingAdapter(Class<T> targetType) {
       XmlDeserializer factoryTypeAnn = targetType.getAnnotation(XmlDeserializer.class);
       if (factoryTypeAnn==null || factoryTypeAnn.value()==null) {
-        throw new IllegalArgumentException("For unmarshalling with this adapter to work, the type must have the "+XmlDeserializer.class.getName()+" annotation");
+        throw new IllegalArgumentException("For unmarshalling with this adapter to work, the type "+targetType.getName()+" must have the "+XmlDeserializer.class.getName()+" annotation");
       }
       try {
         mFactory = factoryTypeAnn.value().newInstance();
@@ -74,7 +121,7 @@ public interface XmlSerializable {
     }
 
     @Override
-    public XmlSerializable unmarshal(final SimpleAdapter v) throws Exception {
+    public T unmarshal(final SimpleAdapter v) throws Exception {
       DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
       dbf.setNamespaceAware(true);
       Document document = dbf.newDocumentBuilder().newDocument();
@@ -82,16 +129,28 @@ public interface XmlSerializable {
       QName outerName = v.name==null ? new QName("value") : v.name;
       Element root;
       root = XmlUtil.createElement(document, outerName);
+
+      SimpleNamespaceContext namespaceContext = v.namespaceContext;
+      for(int i = ((SimpleNamespaceContext) namespaceContext).size()-1; i>=0; --i) {
+        String prefix = namespaceContext.getPrefix(i);
+        String namespace = namespaceContext.getNamespaceURI(i);
+        if (XMLConstants.DEFAULT_NS_PREFIX.equals(prefix)) {
+          root.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, "xmlns" , namespace);
+        } else {
+          root.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, "xmlns:" + prefix, namespace);
+        }
+      }
+
+
       for (Entry<QName, Object> attr: v.attributes.entrySet()) {
         XmlUtil.setAttribute(root, attr.getKey(),(String) attr.getValue());
       }
       for (Object child:v.children) {
         root.appendChild((Node) child);
       }
-
       XMLInputFactory xif = XMLInputFactory.newFactory();
       XMLStreamReader xsr = xif.createXMLStreamReader(new DOMSource(root));
-      return (XmlSerializable) mFactory.deserialize(xsr);
+      return mFactory.deserialize(xsr);
     }
 
   }
