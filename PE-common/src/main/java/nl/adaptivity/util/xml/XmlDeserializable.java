3eb75fa69b8c3926618b500b57275b889af2a7e8
Paul de Vrieze <paul.devrieze@gmail.com>
2015-11-06 11:19:23 +0000

    Fix and simplify the namespace handling code.

--- PE-common/src/main/java/nl/adaptivity/util/xml/XmlDeserializable.java
+++ PE-common/src/main/java/nl/adaptivity/util/xml/XmlDeserializable.java
@@ -1,6 +1,7 @@
 package nl.adaptivity.util.xml;
 
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamReader;
 
 
 /**
@@ -9,6 +10,9 @@ import javax.xml.namespace.QName;
 public interface XmlDeserializable extends XmlSerializable {
 
   boolean deserializeAttribute(String pAttributeNamespace, String pAttributeLocalName, String pAttributeValue);
+
+  /** Listener called just before the children are deserialized. After attributes have been processed. */
+  void onBeforeDeserializeChildren(XMLStreamReader pIn);
 
   QName getElementName();
 }
