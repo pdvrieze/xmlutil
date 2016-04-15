a1e44a10622f7a5c1b3fbbfdb6e3faa1d6e30319
Paul de Vrieze <paul.devrieze@gmail.com>
2016-04-15 20:58:22 +0100

    Convert to kotlin

--- xmlutil/core/src/main/kotlin/nl/adaptivity/xml/XmlDeserializer.kt
+++ xmlutil/core/src/main/kotlin/nl/adaptivity/xml/XmlDeserializer.kt
@@ -14,26 +14,18 @@
  * see <http://www.gnu.org/licenses/>.
  */
 
-package nl.adaptivity.xml;
+package nl.adaptivity.xml
 
-import nl.adaptivity.xml.XmlDeserializerFactory;
-import org.jetbrains.annotations.NotNull;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import kotlin.reflect.KClass
 
 
 /**
  * Annotation that specifies the Deserializer for this type.
- *
+
  * Created by pdvrieze on 27/08/15.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface XmlDeserializer {
-
-  @NotNull Class<? extends XmlDeserializerFactory> value();
-
-}
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+annotation class XmlDeserializer(val value: KClass<out XmlDeserializerFactory<Any>>)
