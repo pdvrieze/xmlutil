982b075b9f39620216476fbd87a2a73a32298dc9
Paul de Vrieze <paul.devrieze@gmail.com>
2017-01-08 20:20:14 +0000

    Convert SimpleXmlDeserializable to Kotlin.

--- xmlutil/core/src/main/kotlin/nl/adaptivity/util/xml/SimpleXmlDeserializable.kt
+++ xmlutil/core/src/main/kotlin/nl/adaptivity/util/xml/SimpleXmlDeserializable.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016.
+ * Copyright (c) 2017.
  *
  * This file is part of ProcessManager.
  *
@@ -14,32 +14,39 @@
  * see <http://www.gnu.org/licenses/>.
  */
 
-package nl.adaptivity.util.xml;
+package nl.adaptivity.util.xml
 
-import nl.adaptivity.xml.XmlDeserializable;
-import nl.adaptivity.xml.XmlException;
-import nl.adaptivity.xml.XmlReader;
+import nl.adaptivity.xml.XmlDeserializable
+import nl.adaptivity.xml.XmlException
+import nl.adaptivity.xml.XmlReader
 
 
 /**
+ * Despite the name it is reasonably sophisticated, but it structures the parsing of the children and
+ * provides them individually. This in contrast to [ExtXmlDeserializable] that provides full access
+ * to parse the content whatever way desired.
  * Created by pdvrieze on 04/11/15.
  */
-public interface SimpleXmlDeserializable extends XmlDeserializable {
+interface SimpleXmlDeserializable : XmlDeserializable {
 
 
   /**
    * Handle the current child element
-   * @param in The reader to read from. It is at the relevant start node.
-   * @return true, if processed, false if not (will trigger an error)
+   * @param reader The reader to read from. It is at the relevant start node.
+   *
+   * @return `true`, if processed, `false` if not (will trigger an error)
+   *
    * @throws XmlException If something else failed.
    */
-  boolean deserializeChild(final XmlReader in) throws XmlException;
+  @Throws(XmlException::class)
+  fun deserializeChild(reader: XmlReader): Boolean = false
 
   /**
    * Handle text content in the node. This may be called multiple times in a single element if there are tags in between
    * or the parser isn't coalescing.
    * @param elementText The read text
+   * *
    * @return true if handled, false if not (whitespace will be ignored later on though, other text will trigger a failure)
    */
-  boolean deserializeChildText(CharSequence elementText);
+  fun deserializeChildText(elementText: CharSequence): Boolean = false
 }
