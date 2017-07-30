0a73357d0a1b34fe5f80a4f881b8f709ab0182ef
Paul de Vrieze <paul.devrieze@gmail.com>
2017-07-30 13:45:41 +0100

    Convert everything else  in xmlutil-core into Kotlin.

--- xmlutil/core/src/main/kotlin/nl/adaptivity/util/xml/ExtXmlDeserializable.kt
+++ xmlutil/core/src/main/kotlin/nl/adaptivity/util/xml/ExtXmlDeserializable.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016.
+ * Copyright (c) 2017.
  *
  * This file is part of ProcessManager.
  *
@@ -14,23 +14,24 @@
  * see <http://www.gnu.org/licenses/>.
  */
 
-package nl.adaptivity.util.xml;
+package nl.adaptivity.util.xml
 
-import nl.adaptivity.xml.XmlDeserializable;
-import nl.adaptivity.xml.XmlException;
-import nl.adaptivity.xml.XmlReader;
+import nl.adaptivity.xml.XmlDeserializable
+import nl.adaptivity.xml.XmlException
+import nl.adaptivity.xml.XmlReader
 
 
 /**
- * Interface that allows more customization on child deserialization than {@link SimpleXmlDeserializable}.
+ * Interface that allows more customization on child deserialization than [SimpleXmlDeserializable].
  * Created by pdvrieze on 04/11/15.
  */
-public interface ExtXmlDeserializable extends XmlDeserializable {
+interface ExtXmlDeserializable : XmlDeserializable {
 
   /**
    * Called to have all children of the current node deserialized. The attributes have already been parsed. The expected
    * end state is that the streamreader is at the corresponding endElement.
    * @param in The streamreader that is the source of the events.
    */
-  void deserializeChildren(XmlReader in) throws XmlException;
+  @Throws(XmlException::class)
+  fun deserializeChildren(`in`: XmlReader)
 }
