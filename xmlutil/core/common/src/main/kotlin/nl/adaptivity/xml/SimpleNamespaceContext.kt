223cf589b4fd672eee9fde7d4b05d9b45805c14f
Paul de Vrieze <paul.devrieze@gmail.com>
2018-05-04 08:47:58 +0100

    Add equals and hashcode. All namespaces are comparable with eachother. Make toString nice as well.

--- xmlutil/core/common/src/main/kotlin/nl/adaptivity/xml/SimpleNamespaceContext.kt
+++ xmlutil/core/common/src/main/kotlin/nl/adaptivity/xml/SimpleNamespaceContext.kt
@@ -29,160 +29,180 @@ import nl.adaptivity.xml.XMLConstants.XML_NS_URI
  * A simple namespace context that stores namespaces in a single array
  * Created by pdvrieze on 24/08/15.
  */
-open class SimpleNamespaceContext internal constructor(val buffer:Array<out String>) : IterableNamespaceContext {
+open class SimpleNamespaceContext internal constructor(val buffer: Array<out String>) : IterableNamespaceContext {
 
-  private inner class SimpleIterator : Iterator<Namespace> {
-    private var pos = 0
+    private inner class SimpleIterator : Iterator<Namespace> {
+        private var pos = 0
 
-    override fun hasNext() = pos < size
+        override fun hasNext() = pos < size
 
-    override fun next(): Namespace = SimpleNamespace(pos++)
-  }
-
-  private inner class SimpleNamespace(private val pos: Int) : Namespace {
-
-    override val prefix: String
-      get() = getPrefix(pos)
-
-    override val namespaceURI: String
-      get() = getNamespaceURI(pos)
-  }
-
-  constructor(): this(emptyArray())
-
-  constructor(prefixMap: Map<out CharSequence, CharSequence>):
-    this(flatten(prefixMap.entries, { key.toString() }, { value.toString() }))
-
-  constructor(prefixes: Array<out CharSequence>, namespaces: Array<out CharSequence>):
-  this(Array(prefixes.size*2, { (if (it %2==0) prefixes[it/2] else namespaces[it/2]).toString() }))
-
-  constructor(prefix: CharSequence, namespace: CharSequence):
-    this(arrayOf(prefix.toString(),namespace.toString()))
-
-  constructor(namespaces: Collection<Namespace>):
-    this(flatten(namespaces, { prefix }, { namespaceURI }))
-
-  constructor(namespaces: Iterable<Namespace>):
-    this(namespaces as? Collection<Namespace> ?: namespaces.toList())
-
-  val indices:IntRange get() = 0..(size-1)
-
-  @get:JvmName("size")
-  val size:Int
-    get() = buffer.size / 2
-
-  /**
-   * Create a context that combines both. This will "forget" overlapped prefixes.
-   */
-  fun combine(other: SimpleNamespaceContext): SimpleNamespaceContext {
-    val result = mutableMapOf<String, String>()
-    for (i in indices.reversed()) {
-      result.put(getPrefix(i), getNamespaceURI(i))
+        override fun next(): Namespace = SimpleNamespace(pos++)
     }
-    for (i in other.indices.reversed()) {
-      result.put(other.getPrefix(i), other.getNamespaceURI(i))
+
+    private inner class SimpleNamespace(private val pos: Int) : Namespace {
+
+        override val prefix: String
+            get() = getPrefix(pos)
+
+        override val namespaceURI: String
+            get() = getNamespaceURI(pos)
+
+        override fun hashCode(): Int {
+            return prefix.hashCode() * 31 + namespaceURI.hashCode()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Namespace) return false
+
+            return prefix == other.prefix && namespaceURI == other.namespaceURI
+        }
+
+        override fun toString(): String {
+            return "{$prefix:$namespaceURI}"
+        }
     }
-    return SimpleNamespaceContext(result)
-  }
 
-  /**
-   * Combine this context with the additional context. The prefixes already in this context prevail over the added ones.
-   * @param other The namespaces to add
-   *
-   * @return the new context
-   */
-  fun combine(other: Iterable<Namespace>): SimpleNamespaceContext? {
-    if (size == 0) {
-      return from(other)
+    constructor() : this(emptyArray())
+
+    constructor(prefixMap: Map<out CharSequence, CharSequence>) :
+        this(flatten(prefixMap.entries, { key.toString() }, { value.toString() }))
+
+    constructor(prefixes: Array<out CharSequence>, namespaces: Array<out CharSequence>) :
+        this(Array(prefixes.size * 2, { (if (it % 2 == 0) prefixes[it / 2] else namespaces[it / 2]).toString() }))
+
+    constructor(prefix: CharSequence, namespace: CharSequence) :
+        this(arrayOf(prefix.toString(), namespace.toString()))
+
+    constructor(namespaces: Collection<Namespace>) :
+        this(flatten(namespaces, { prefix }, { namespaceURI }))
+
+    constructor(namespaces: Iterable<Namespace>) :
+        this(namespaces as? Collection<Namespace> ?: namespaces.toList())
+
+    val indices: IntRange get() = 0..(size - 1)
+
+    @get:JvmName("size")
+    val size: Int
+        get() = buffer.size / 2
+
+    /**
+     * Create a context that combines both. This will "forget" overlapped prefixes.
+     */
+    fun combine(other: SimpleNamespaceContext): SimpleNamespaceContext {
+        val result = mutableMapOf<String, String>()
+        for (i in indices.reversed()) {
+            result.put(getPrefix(i), getNamespaceURI(i))
+        }
+        for (i in other.indices.reversed()) {
+            result.put(other.getPrefix(i), other.getNamespaceURI(i))
+        }
+        return SimpleNamespaceContext(result)
     }
-    if (other is SimpleNamespaceContext) { return combine(other) }
-    else if (!other.iterator().hasNext()) {
-      return this
+
+    /**
+     * Combine this context with the additional context. The prefixes already in this context prevail over the added ones.
+     * @param other The namespaces to add
+     *
+     * @return the new context
+     */
+    fun combine(other: Iterable<Namespace>): SimpleNamespaceContext? {
+        if (size == 0) {
+            return from(other)
+        }
+        if (other is SimpleNamespaceContext) {
+            return combine(other)
+        } else if (!other.iterator().hasNext()) {
+            return this
+        }
+        val result = mutableMapOf<String, String>()
+        for (i in indices.reversed()) {
+            result.put(getPrefix(i), getNamespaceURI(i))
+        }
+        for (ns in other) {
+            result.put(ns.prefix, ns.namespaceURI)
+        }
+        return SimpleNamespaceContext(result)
+
     }
-    val result = mutableMapOf<String, String>()
-    for (i in indices.reversed()) {
-      result.put(getPrefix(i), getNamespaceURI(i))
+
+    override fun getNamespaceURI(prefix: String): String {
+        return when (prefix) {
+            XML_NS_PREFIX   -> XML_NS_URI
+            XMLNS_ATTRIBUTE -> XMLNS_ATTRIBUTE_NS_URI
+            else            -> indices.reversed()
+                                   .filter { getPrefix(it) == prefix }
+                                   .map { getNamespaceURI(it) }
+                                   .firstOrNull() ?: NULL_NS_URI
+        }
     }
-    for (ns in other) {
-      result.put(ns.prefix, ns.namespaceURI)
+
+    override fun getPrefix(namespaceURI: String) = getPrefixSequence(namespaceURI).firstOrNull()
+
+    fun getPrefixSequence(namespaceURI: String): Sequence<String> {
+        return when (namespaceURI) {
+            XML_NS_URI             -> sequenceOf(XML_NS_PREFIX)
+            NULL_NS_URI            -> sequenceOf(DEFAULT_NS_PREFIX)
+            XMLNS_ATTRIBUTE_NS_URI -> sequenceOf(XMLNS_ATTRIBUTE)
+            else                   -> {
+                indices.reversed().asSequence()
+                    .filter { getNamespaceURI(it) == namespaceURI }
+                    .map { getPrefix(it) }
+            }
+        }
     }
-    return SimpleNamespaceContext(result)
 
-  }
+    @Suppress("OverridingDeprecatedMember")
+    override fun getPrefixes(namespaceURI: String) = getPrefixSequence(namespaceURI).iterator()
 
-  override fun getNamespaceURI(prefix: String): String {
-    return when (prefix) {
-      XML_NS_PREFIX   -> XML_NS_URI
-      XMLNS_ATTRIBUTE -> XMLNS_ATTRIBUTE_NS_URI
-      else -> indices.reversed()
-            .filter {getPrefix(it)==prefix}
-            .map { getNamespaceURI(it) }
-            .firstOrNull()?: NULL_NS_URI
+    fun getPrefix(index: Int): String {
+        try {
+            return buffer[index * 2]
+        } catch (e: IndexOutOfBoundsException) {
+            throw IndexOutOfBoundsException("Index out of range: $index")
+        }
     }
-  }
 
-  override fun getPrefix(namespaceURI: String) = getPrefixSequence(namespaceURI).firstOrNull()
-
-  fun getPrefixSequence(namespaceURI: String):Sequence<String> {
-    return when (namespaceURI) {
-      XML_NS_URI             -> sequenceOf(XML_NS_PREFIX)
-      NULL_NS_URI            -> sequenceOf(DEFAULT_NS_PREFIX)
-      XMLNS_ATTRIBUTE_NS_URI -> sequenceOf(XMLNS_ATTRIBUTE)
-      else                   -> {
-        indices.reversed().asSequence()
-              .filter { getNamespaceURI(it)== namespaceURI }
-              .map { getPrefix(it) }
-      }
+    fun getNamespaceURI(index: Int): String {
+        try {
+            return buffer[index * 2 + 1]
+        } catch (e: IndexOutOfBoundsException) {
+            throw IndexOutOfBoundsException("Index out of range: $index")
+        }
     }
-  }
 
-  @Suppress("OverridingDeprecatedMember")
-  override fun getPrefixes(namespaceURI: String) = getPrefixSequence(namespaceURI).iterator()
-
-  fun getPrefix(index: Int): String {
-    try {
-      return buffer[index * 2]
-    } catch (e: IndexOutOfBoundsException) {
-      throw IndexOutOfBoundsException("Index out of range: $index")
+    override fun iterator(): Iterator<Namespace> {
+        return SimpleIterator()
     }
-  }
 
-  fun getNamespaceURI(index: Int): String {
-    try {
-      return buffer[index * 2 + 1]
-    } catch (e: IndexOutOfBoundsException) {
-      throw IndexOutOfBoundsException("Index out of range: $index")
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is SimpleNamespaceContext) return false
+
+        if (!buffer.contentEquals(other.buffer)) return false
+
+        return true
     }
-  }
 
-  override fun iterator(): Iterator<Namespace> {
-    return SimpleIterator()
-  }
-
-  override fun equals(other: Any?): Boolean{
-    if (this === other) return true
-    if (other !is SimpleNamespaceContext) return false
-
-    if (!buffer.contentEquals(other.buffer)) return false
-
-    return true
-  }
-
-  override fun hashCode(): Int{
-    return buffer.contentHashCode()
-  }
-
-  companion object {
-
-    fun from(originalNSContext: Iterable<Namespace>): SimpleNamespaceContext = when (originalNSContext) {
-        is SimpleNamespaceContext -> originalNSContext
-        else                                        -> SimpleNamespaceContext(originalNSContext)
+    override fun hashCode(): Int {
+        return buffer.contentHashCode()
     }
-  }
+
+    companion object {
+
+        fun from(originalNSContext: Iterable<Namespace>): SimpleNamespaceContext = when (originalNSContext) {
+            is SimpleNamespaceContext -> originalNSContext
+            else                      -> SimpleNamespaceContext(originalNSContext)
+        }
+    }
 }
 
 
-private inline fun <T> flatten(namespaces: Collection<T>, crossinline prefix:T.()->String, crossinline namespace:T.()->String):Array<String> {
-  val filler:Iterator<String> = namespaces.asSequence().flatMap { sequenceOf(it.prefix(), it.namespace()) }.iterator()
-  return Array(namespaces.size*2, { filler.next() })
+private inline fun <T> flatten(namespaces: Collection<T>,
+                               crossinline prefix: T.() -> String,
+                               crossinline namespace: T.() -> String): Array<String> {
+    val filler: Iterator<String> = namespaces.asSequence().flatMap {
+        sequenceOf(it.prefix(), it.namespace())
+    }.iterator()
+    return Array(namespaces.size * 2, { filler.next() })
 }
--- xmlutil/core/common/src/main/kotlin/nl/adaptivity/xml/XmlEvent.kt
+++ xmlutil/core/common/src/main/kotlin/nl/adaptivity/xml/XmlEvent.kt
@@ -21,179 +21,202 @@ package nl.adaptivity.xml
  *
  * Created by pdvrieze on 16/11/15.
  */
-sealed  class XmlEvent private constructor(val locationInfo: String?) {
+sealed class XmlEvent private constructor(val locationInfo: String?) {
 
-  companion object {
-    internal val IGNORABLE = object: AbstractSet<EventType>() {
-      private val elements = BooleanArray(EventType.values().size)
+    companion object {
+        internal val IGNORABLE = object : AbstractSet<EventType>() {
+            private val elements = BooleanArray(EventType.values().size)
 
-      init {
-        elements[EventType.COMMENT.ordinal]=true
-        elements[EventType.START_DOCUMENT.ordinal]=true
-        elements[EventType.END_DOCUMENT.ordinal]=true
-        elements[EventType.PROCESSING_INSTRUCTION.ordinal]=true
-        elements[EventType.DOCDECL.ordinal]=true
-        elements[EventType.IGNORABLE_WHITESPACE.ordinal]=true
-      }
+            init {
+                elements[EventType.COMMENT.ordinal] = true
+                elements[EventType.START_DOCUMENT.ordinal] = true
+                elements[EventType.END_DOCUMENT.ordinal] = true
+                elements[EventType.PROCESSING_INSTRUCTION.ordinal] = true
+                elements[EventType.DOCDECL.ordinal] = true
+                elements[EventType.IGNORABLE_WHITESPACE.ordinal] = true
+            }
 
-      override fun contains(element: EventType) = elements[element.ordinal]
+            override fun contains(element: EventType) = elements[element.ordinal]
 
-      override val size get() = 6
+            override val size get() = 6
 
-      override fun iterator(): Iterator<EventType>
-        = elements.mapIndexed { index, b -> if (b) EventType.values()[index] else null }.filterNotNull().iterator()
+            override fun iterator(): Iterator<EventType> = elements.mapIndexed { index, b -> if (b) EventType.values()[index] else null }.filterNotNull().iterator()
+        }
+
+
+        fun from(reader: XmlReader) = reader.eventType.createEvent(reader)
+
+        @Deprecated("Use the extension property", ReplaceWith("reader.namespaceDecls", "nl.adaptivity.xml.attributes"))
+        internal fun getNamespaceDecls(reader: XmlReader): Array<out Namespace> {
+            val readerOffset = reader.namespaceStart
+            val namespaces = Array<Namespace>(reader.namespaceEnd - readerOffset) { i ->
+                val nsIndex = readerOffset + i
+                NamespaceImpl(reader.getNamespacePrefix(nsIndex), reader.getNamespaceUri(nsIndex))
+            }
+            return namespaces
+        }
+
+        @Deprecated("Use the extension property", ReplaceWith("reader.attributes", "nl.adaptivity.xml.attributes"))
+        internal fun getAttributes(reader: XmlReader): Array<out Attribute> {
+            val result = Array(reader.attributeCount) { i ->
+                Attribute(reader.locationInfo,
+                          reader.getAttributeNamespace(i),
+                          reader.getAttributeLocalName(i),
+                          reader.getAttributePrefix(i),
+                          reader.getAttributeValue(i))
+            }
+
+            return result
+        }
+
     }
 
-
-    fun from(reader: XmlReader) = reader.eventType.createEvent(reader)
-
-    @Deprecated("Use the extension property", ReplaceWith("reader.namespaceDecls", "nl.adaptivity.xml.attributes"))
-    internal fun getNamespaceDecls(reader: XmlReader): Array<out Namespace> {
-      val readerOffset = reader.namespaceStart
-      val namespaces = Array<Namespace>(reader.namespaceEnd -readerOffset) { i ->
-        val nsIndex = readerOffset + i
-          NamespaceImpl(reader.getNamespacePrefix(nsIndex), reader.getNamespaceUri(nsIndex))
-      }
-      return namespaces
-    }
-
-    @Deprecated("Use the extension property", ReplaceWith("reader.attributes", "nl.adaptivity.xml.attributes"))
-    internal fun getAttributes(reader: XmlReader): Array<out Attribute> {
-      val result = Array(reader.attributeCount) { i ->
-          Attribute(reader.locationInfo,
-                                               reader.getAttributeNamespace(i),
-                                               reader.getAttributeLocalName(i),
-                                               reader.getAttributePrefix(i),
-                                               reader.getAttributeValue(i))
-      }
-
-      return result
-    }
-
-  }
-
-  class TextEvent(locationInfo: String?, override val eventType: EventType, val text: CharSequence) : XmlEvent(
+    class TextEvent(locationInfo: String?, override val eventType: EventType, val text: CharSequence) : XmlEvent(
         locationInfo) {
 
-    override fun writeTo(writer: XmlWriter) = eventType.writeEvent(writer, this)
+        override fun writeTo(writer: XmlWriter) = eventType.writeEvent(writer, this)
 
-    override val isIgnorable: Boolean
-      get() =
-        super.isIgnorable || (eventType == EventType.TEXT && isXmlWhitespace(text))
+        override val isIgnorable: Boolean
+            get() =
+                super.isIgnorable || (eventType == EventType.TEXT && isXmlWhitespace(text))
 
-  }
-
-  class EndDocumentEvent(locationInfo: String?) : XmlEvent(locationInfo) {
-
-    override fun writeTo(writer: XmlWriter) = writer.endDocument()
-
-    override val eventType: EventType get() = EventType.END_DOCUMENT
-  }
-
-  class EndElementEvent(locationInfo: String?, namespaceUri: CharSequence, localName: CharSequence, prefix: CharSequence) :
-      NamedEvent(locationInfo, namespaceUri, localName, prefix) {
-
-    override fun writeTo(writer: XmlWriter) = writer.endTag(namespaceUri, localName, prefix)
-
-    override val eventType: EventType get() = EventType.END_ELEMENT
-  }
-
-  class StartDocumentEvent(locationInfo: String?,
-                           val version: CharSequence?,
-                           val encoding: CharSequence?,
-                           val standalone: Boolean?) :
-      XmlEvent(locationInfo) {
-
-    override fun writeTo(writer: XmlWriter) = writer.startDocument(version, encoding, standalone)
-
-    override val eventType: EventType get() = EventType.START_DOCUMENT
-  }
-
-  abstract class NamedEvent(locationInfo: String?,
-                                    val namespaceUri: CharSequence,
-                                    val localName: CharSequence,
-                                    val prefix: CharSequence) :
-      XmlEvent(locationInfo) {
-
-    fun isEqualNames(ev: NamedEvent): Boolean {
-        return namespaceUri.toString()==ev.namespaceUri.toString() &&
-               localName.toString()==ev.localName.toString() &&
-               prefix.toString()==ev.prefix.toString()
     }
 
-  }
+    class EndDocumentEvent(locationInfo: String?) : XmlEvent(locationInfo) {
 
-  class StartElementEvent(locationInfo: String?,
+        override fun writeTo(writer: XmlWriter) = writer.endDocument()
+
+        override val eventType: EventType get() = EventType.END_DOCUMENT
+    }
+
+    class EndElementEvent(locationInfo: String?,
                           namespaceUri: CharSequence,
                           localName: CharSequence,
-                          prefix: CharSequence,
-                          val attributes: Array<out Attribute>,
-                          val namespaceDecls: Array<out Namespace>) :
-      NamedEvent(locationInfo, namespaceUri, localName, prefix), NamespaceContext {
+                          prefix: CharSequence) :
+        NamedEvent(locationInfo, namespaceUri, localName, prefix) {
 
-    override fun writeTo(writer: XmlWriter) {
-      writer.startTag(namespaceUri, localName, prefix)
+        override fun writeTo(writer: XmlWriter) = writer.endTag(namespaceUri, localName, prefix)
 
-      attributes.forEach { attr -> writer.attribute(attr.namespaceUri, attr.localName, attr.prefix, attr.value) }
-
-      namespaceDecls.forEach { ns -> writer.namespaceAttr(ns.prefix, ns.namespaceURI) }
+        override val eventType: EventType get() = EventType.END_ELEMENT
     }
 
-    override val eventType: EventType get() = EventType.START_ELEMENT
+    class StartDocumentEvent(locationInfo: String?,
+                             val version: CharSequence?,
+                             val encoding: CharSequence?,
+                             val standalone: Boolean?) :
+        XmlEvent(locationInfo) {
 
-    override fun getPrefix(namespaceURI: String) = getPrefix(namespaceURI as CharSequence)
+        override fun writeTo(writer: XmlWriter) = writer.startDocument(version, encoding, standalone)
 
-    fun getPrefix(namespaceUri: CharSequence): String? {
-      return namespaceDecls
-            .asSequence()
-            .filter { ns -> ns.namespaceURI.toString() == namespaceUri.toString() }
-            .lastOrNull()?.prefix
+        override val eventType: EventType get() = EventType.START_DOCUMENT
     }
 
-    override fun getNamespaceURI(prefix: String) = getNamespaceUri(prefix)
+    abstract class NamedEvent(locationInfo: String?,
+                              val namespaceUri: CharSequence,
+                              val localName: CharSequence,
+                              val prefix: CharSequence) :
+        XmlEvent(locationInfo) {
 
-    fun getNamespaceUri(prefix: CharSequence): String? {
-      return namespaceDecls
-            .asSequence()
-            .filter { ns -> ns.prefix ==prefix.toString() }
-            .lastOrNull()?.namespaceURI
+        fun isEqualNames(ev: NamedEvent): Boolean {
+            return namespaceUri.toString() == ev.namespaceUri.toString() &&
+                   localName.toString() == ev.localName.toString() &&
+                   prefix.toString() == ev.prefix.toString()
+        }
+
     }
 
-    val namespaceContext: NamespaceContext get() = this
+    class StartElementEvent(locationInfo: String?,
+                            namespaceUri: CharSequence,
+                            localName: CharSequence,
+                            prefix: CharSequence,
+                            val attributes: Array<out Attribute>,
+                            val namespaceDecls: Array<out Namespace>) :
+        NamedEvent(locationInfo, namespaceUri, localName, prefix), NamespaceContext {
 
-    override fun getPrefixes(namespaceURI: String): Iterator<String> {
-      return namespaceDecls
-            .asSequence()
-            .filter { ns -> ns.namespaceURI == namespaceUri.toString() }
-            .map { it.prefix }.iterator()
+        override fun writeTo(writer: XmlWriter) {
+            writer.startTag(namespaceUri, localName, prefix)
+
+            attributes.forEach { attr -> writer.attribute(attr.namespaceUri, attr.localName, attr.prefix, attr.value) }
+
+            namespaceDecls.forEach { ns -> writer.namespaceAttr(ns.prefix, ns.namespaceURI) }
+        }
+
+        override val eventType: EventType get() = EventType.START_ELEMENT
+
+        override fun getPrefix(namespaceURI: String) = getPrefix(namespaceURI as CharSequence)
+
+        fun getPrefix(namespaceUri: CharSequence): String? {
+            return namespaceDecls
+                .asSequence()
+                .filter { ns -> ns.namespaceURI.toString() == namespaceUri.toString() }
+                .lastOrNull()?.prefix
+        }
+
+        override fun getNamespaceURI(prefix: String) = getNamespaceUri(prefix)
+
+        fun getNamespaceUri(prefix: CharSequence): String? {
+            return namespaceDecls
+                .asSequence()
+                .filter { ns -> ns.prefix == prefix.toString() }
+                .lastOrNull()?.namespaceURI
+        }
+
+        val namespaceContext: NamespaceContext get() = this
+
+        override fun getPrefixes(namespaceURI: String): Iterator<String> {
+            return namespaceDecls
+                .asSequence()
+                .filter { ns -> ns.namespaceURI == namespaceUri.toString() }
+                .map { it.prefix }.iterator()
+        }
     }
-  }
 
-  class Attribute(locationInfo: String?, val namespaceUri: CharSequence, val localName: CharSequence, val prefix: CharSequence, val value: CharSequence) : XmlEvent(
+    class Attribute(locationInfo: String?,
+                    val namespaceUri: CharSequence,
+                    val localName: CharSequence,
+                    val prefix: CharSequence,
+                    val value: CharSequence) : XmlEvent(
         locationInfo) {
 
-    override val eventType: EventType get() = EventType.ATTRIBUTE
+        override val eventType: EventType get() = EventType.ATTRIBUTE
 
-    override fun writeTo(writer: XmlWriter) = writer.attribute(namespaceUri, localName, prefix, value)
+        override fun writeTo(writer: XmlWriter) = writer.attribute(namespaceUri, localName, prefix, value)
 
-    fun hasNamespaceUri(): Boolean {
-      return XMLConstants.XMLNS_ATTRIBUTE_NS_URI == namespaceUri.toString() ||
-             (prefix.isEmpty() && XMLConstants.XMLNS_ATTRIBUTE == localName.toString())
+        fun hasNamespaceUri(): Boolean {
+            return XMLConstants.XMLNS_ATTRIBUTE_NS_URI == namespaceUri.toString() ||
+                   (prefix.isEmpty() && XMLConstants.XMLNS_ATTRIBUTE == localName.toString())
+        }
     }
-  }
 
-  class NamespaceImpl(private val namespacePrefix: CharSequence, private val namespaceUri: CharSequence) : Namespace {
+    class NamespaceImpl(private val namespacePrefix: CharSequence, private val namespaceUri: CharSequence) : Namespace {
 
-      override val prefix: String
-          get() = namespacePrefix.toString()
-      override val namespaceURI get() = namespaceUri.toString()
+        override val prefix: String get() = namespacePrefix.toString()
 
-  }
+        override val namespaceURI get() = namespaceUri.toString()
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Namespace) return false
 
-  abstract val eventType: EventType
+            if (namespacePrefix != other.prefix) return false
+            if (namespaceUri != other.namespaceURI) return false
 
-  open val isIgnorable:Boolean get() = eventType.isIgnorable
+            return true
+        }
 
-  abstract fun writeTo(writer: XmlWriter)
+        override fun hashCode(): Int {
+            return 31 * namespacePrefix.hashCode() + namespaceUri.hashCode()
+        }
+
+        override fun toString(): String {
+            return "{$prefix:$namespaceURI}"
+        }
+
+    }
+
+    abstract val eventType: EventType
+
+    open val isIgnorable: Boolean get() = eventType.isIgnorable
+
+    abstract fun writeTo(writer: XmlWriter)
 
 }
