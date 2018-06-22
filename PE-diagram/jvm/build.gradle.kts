8cc3afb391c1bfd4325b7988c89f2ef248c2324a
Paul de Vrieze <paul.devrieze@gmail.com>
2018-06-22 05:59:12 +0100

    Fix the build system. Use a configurable android target. Make sure to pass the jvmdefault parameter properly.

--- PE-diagram/jvm/build.gradle.kts
+++ PE-diagram/jvm/build.gradle.kts
@@ -13,22 +13,8 @@
  * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
  * see <http://www.gnu.org/licenses/>.
  */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-/*
- * Copyright (c) 2018.
- *
- * This file is part of ProcessManager.
- *
- * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
- * GNU Lesser General Public License as published by the Free Software Foundation.
- *
- * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
- * see <http://www.gnu.org/licenses/>.
- */
 
 plugins {
     `java-library`
@@ -65,4 +51,11 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val argJvmDefault:String by project
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.freeCompilerArgs=listOf(argJvmDefault)
+    kotlinOptions.jvmTarget = "1.8"
 }
