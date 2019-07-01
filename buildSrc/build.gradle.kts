9c6704d202ab501164c47b09d52e592372ca62e8
Paul de Vrieze <paul.devrieze@gmail.com>
2019-07-01 03:48:25 +0100

    Include a patch that ensures that module files are published to bintray where applicable (instead of this having to be done manually).

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -39,7 +39,7 @@ val bintrayVersion: String by project
 val kotlin_version: String by project
 
 dependencies {
-    compileOnly("com.jfrog.bintray.gradle:gradle-bintray-plugin:$bintrayVersion")
+    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:$bintrayVersion")
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 }
 
