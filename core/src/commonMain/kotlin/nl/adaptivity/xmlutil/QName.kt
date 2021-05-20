f66e3dcecf6bcab762329a8db1238593b42bac90
Paul de Vrieze <paul.devrieze@gmail.com>
2021-05-20 09:24:18 +0100

    Add a serializer for QNames so that XML serialization can special case it.

--- core/src/commonMain/kotlin/nl/adaptivity/xmlutil/QName.kt
+++ core/src/commonMain/kotlin/nl/adaptivity/xmlutil/QName.kt
@@ -20,6 +20,14 @@
 
 package nl.adaptivity.xmlutil
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.*
+
 expect class QName {
     constructor(namespaceURI: String, localPart: String, prefix: String)
     constructor(namespaceURI: String, localPart: String)
@@ -36,4 +44,49 @@ inline val QName.namespaceURI get() = getNamespaceURI()
 
 fun QName.toNamespace(): Namespace {
     return XmlEvent.NamespaceImpl(prefix, namespaceURI)
+}
+
+@Serializer(forClass = QName::class)
+object QNameSerializer : KSerializer<QName> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("javax.xml.namespace.QName") {
+        val stringSerializer = String.serializer()
+        element("namespace", stringSerializer.descriptor, isOptional = true)
+        element("localPart", stringSerializer.descriptor)
+        element("prefix", stringSerializer.descriptor, isOptional = true)
+    }
+
+    override fun deserialize(decoder: Decoder): QName = decoder.decodeStructure(descriptor) {
+        var prefix = ""
+        var namespace = ""
+        lateinit var localPart: String
+
+        loop@while (true) {
+            when (val idx=this.decodeElementIndex(descriptor)) {
+                CompositeDecoder.DECODE_DONE -> break@loop
+                0 -> namespace = decodeStringElement(descriptor, 0)
+                1 -> localPart = decodeStringElement(descriptor, 1)
+                2 -> prefix = decodeStringElement(descriptor, 2)
+            }
+        }
+        return QName(namespace, localPart, prefix)
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun serialize(encoder: Encoder, value: QName) {
+        encoder.encodeStructure(descriptor) {
+            value.namespaceURI.let { ns ->
+                if (ns.isNotEmpty() || shouldEncodeElementDefault(descriptor, 0)) {
+                    encodeStringElement(descriptor, 0, ns)
+                }
+            }
+
+            encodeStringElement(descriptor, 1, value.localPart)
+
+            value.prefix.let { prefix ->
+                if (prefix.isNotEmpty() || shouldEncodeElementDefault(descriptor, 2)) {
+                    encodeStringElement(descriptor, 2, prefix)
+                }
+            }
+        }
+    }
 }
