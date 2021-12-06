680657fcfd7dd860a1d6963e8010b35ff9804e8c
Paul de Vrieze <paul.devrieze@gmail.com>
2021-12-06 22:50:37 +0000

    In stringSets don't allow empty strings (created on splitting empty string). 944 errors.

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/datatypes/serialization/XSKeyref.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/datatypes/serialization/XSKeyref.kt
@@ -14,13 +14,31 @@
  * see <http://www.gnu.org/licenses/>.
  */
 
+@file:UseSerializers(QNameSerializer::class)
+
 package io.github.pdvrieze.formats.xmlschema.datatypes.serialization
 
 import io.github.pdvrieze.formats.xmlschema.XmlSchemaConstants
+import io.github.pdvrieze.formats.xmlschema.datatypes.ID
+import io.github.pdvrieze.formats.xmlschema.datatypes.NCName
 import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.groups.G_IdentityConstraint
+import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.types.T_Keybase
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import nl.adaptivity.xmlutil.QName
+import nl.adaptivity.xmlutil.QNameSerializer
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 
 @Serializable
 @XmlSerialName("keyref", XmlSchemaConstants.XS_NAMESPACE, XmlSchemaConstants.XS_PREFIX)
-class XSKeyref: G_IdentityConstraint.Keyref
+class XSKeyref(
+    override val selector: XSSelector? = null,
+    override val fields: List<XSField> = emptyList(),
+    override val name: NCName? = null,
+    override val ref: QName? = null,
+    val refer: QName? = null,
+    override val id: ID? = null,
+    override val annotations: List<XSAnnotation>,
+    override val otherAttrs: Map<QName, String>
+
+): G_IdentityConstraint.Keyref, T_Keybase
