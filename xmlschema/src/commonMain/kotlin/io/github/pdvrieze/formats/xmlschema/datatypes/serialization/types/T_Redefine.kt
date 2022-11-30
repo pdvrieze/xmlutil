4b6def92000fa4ce21110a866d8b1b43e6ac6f90
Paul de Vrieze <paul.devrieze@gmail.com>
2022-11-30 14:44:51 +0000

    Implement checking of sun 003b with missing type in redefined attribute

---
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/datatypes/serialization/types/T_Redefine.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022.
+ *
+ * This file is part of xmlutil.
+ *
+ * This file is licenced to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You should have received a copy of the license with the source distribution.
+ * Alternatively, you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.github.pdvrieze.formats.xmlschema.datatypes.serialization.types
+
+import io.github.pdvrieze.formats.xmlschema.datatypes.primitiveInstances.VAnyURI
+import io.github.pdvrieze.formats.xmlschema.datatypes.primitiveInstances.VID
+import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.*
+import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.groups.G_Composition
+import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.QName
+import nl.adaptivity.xmlutil.QNameSerializer
+
+interface T_Redefine : T_Annotated, G_Composition.Redefine {
+    val simpleTypes: List<T_TopLevelSimpleType>
+    val complexTypes: List<T_TopLevelComplexType_Base>
+    val groups: List<T_NamedGroup>
+    val attributeGroups: List<T_NamedAttributeGroup>
+    val schemaLocation: VAnyURI
+    override val id: VID?
+    override val annotations: List<XSAnnotation>
+
+    override val otherAttrs: Map<@Serializable(with = QNameSerializer::class) QName, String>
+}
