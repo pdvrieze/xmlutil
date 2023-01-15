264071b9e67e5d3b5eaea8bd8ae6227a7a4ee274
Paul de Vrieze <paul.devrieze@gmail.com>
2023-01-15 21:25:14 +0000

    Let the serializer omit default values so that absense of values can be tested (somewhat).

--- xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
+++ xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
@@ -22,8 +22,12 @@ package io.github.pdvrieze.formats.xmlschema.test
 
 import io.github.pdvrieze.formats.xmlschema.XmlSchemaConstants
 import io.github.pdvrieze.formats.xmlschema.datatypes.primitiveInstances.VAnyURI
-import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.XSSchema
+import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.*
+import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.types.XSI_OpenAttrs
 import io.github.pdvrieze.formats.xmlschema.resolved.SimpleResolver
+import io.github.pdvrieze.formats.xmlschema.test.TestXSTestSuite.NON_TESTED.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
 import nl.adaptivity.xmlutil.QName
 import nl.adaptivity.xmlutil.XmlReader
 import nl.adaptivity.xmlutil.XmlStreaming
@@ -88,11 +92,11 @@ class TestXSTestSuite {
                         schemaTest.schemaDocuments
                     }.map { schemaDoc ->
                         setBaseUrl.resolve(schemaDoc.href).toURL()
-//                    }.filter {
-//                        "/ipo.xsd" in it.path
+                    }.filter {
+                        "particlesIc006.xsd" in it.path
                     }
                 }
-//                assertEquals(1, schemaUrls.size)
+                assertTrue(schemaUrls.size > 0, "Expected at least 1 schema, found 0")
                 val schemas: Sequence<XSSchema> = schemaUrls.asSequence().map { url ->
                     url.withXmlReader { reader ->
                         xml.decodeFromReader<XSSchema>(reader)
@@ -138,7 +142,7 @@ class TestXSTestSuite {
                 is XmlListDescriptor,
                 is XmlMapDescriptor -> {
                     if (elementDescriptor.canRecurse()
-                        ) {
+                    ) {
                         recurse.add(elementDescriptor)
                     }
                 }
@@ -154,33 +158,211 @@ class TestXSTestSuite {
         return collector
     }
 
+    private enum class NON_TESTED(val testSeen: Boolean, val testAbsent: Boolean) {
+        SEEN(false, true),
+        ABSENT(true, false),
+        BOTH(false, false),
+        NONE(true, true)
+    }
+
+    private object TAG_ABSENT : Map<String, NON_TESTED> {
+        override val entries: Set<Map.Entry<String, NON_TESTED>> get() = emptySet()
+        override val keys: Set<String>
+            get() = emptySet()
+        override val size: Int
+            get() = 1
+        override val values: Collection<NON_TESTED>
+            get() = listOf(BOTH)
+
+        override fun isEmpty(): Boolean { return false }
+
+        override fun get(key: String): NON_TESTED = BOTH
+
+        override fun containsValue(value: NON_TESTED): Boolean {
+            return value == BOTH
+        }
+
+        override fun containsKey(key: String): Boolean = true
+
+    }
+
+    private val expectedNonTested: Map<KSerializer<out XSI_OpenAttrs>, Map<String, NON_TESTED>> = mapOf(
+        XSSchema.serializer() to mapOf(
+            "defaultAttributes" to SEEN,
+            "xpathDefaultNamespace" to SEEN,
+            "overrides" to SEEN,
+            "defaultOpenContent" to SEEN,
+        ),
+        XSDocumentation.serializer() to mapOf("content" to ABSENT),
+        XSAppInfo.serializer() to mapOf("content" to ABSENT),
+        XSSimpleRestriction.serializer() to mapOf("otherContents" to SEEN),
+        XSAssertionFacet.serializer() to TAG_ABSENT,
+        XSExplicitTimezone.serializer() to TAG_ABSENT,
+        XSFractionDigits.serializer() to mapOf(
+            "annotations" to SEEN,
+        ),
+        XSLength.serializer() to mapOf(
+            "id" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMaxExclusive.serializer() to mapOf(
+            "fixed" to SEEN,
+            "id" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMaxInclusive.serializer() to mapOf(
+            "fixed" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMaxLength.serializer() to mapOf(
+            "id" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMinExclusive.serializer() to mapOf(
+            "id" to SEEN,
+            "fixed" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMinInclusive.serializer() to mapOf(
+            "fixed" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSMinLength.serializer() to mapOf("annotations" to SEEN),
+        XSTotalDigits.serializer() to mapOf(
+            "fixed" to SEEN,
+            "id" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSWhiteSpace.serializer() to mapOf("annotations" to SEEN),
+        XSTopLevelComplexType.serializer() to mapOf(
+            "abstract" to ABSENT,
+            "asserts" to SEEN,
+            "defaultAttributesApply" to SEEN,
+            "openContents" to SEEN,
+        ),
+        XSAnyAttribute.serializer() to mapOf(
+            "notQName" to SEEN,
+            "notNamespace" to SEEN,
+            "processContents" to ABSENT,
+        ),
+        XSAssert.serializer() to mapOf(
+            "id" to SEEN,
+            "asserts" to SEEN,
+            "openContents" to SEEN,
+        ),
+        XSComplexContent.XSExtension.serializer() to mapOf(
+            "openContents" to SEEN,
+        ),
+        XSComplexContent.XSRestriction.serializer() to mapOf(
+            "id" to SEEN,
+            "annotations" to SEEN,
+            "asserts" to SEEN,
+            "openContents" to SEEN,
+            "simpleTypes" to SEEN,
+            "facets" to SEEN,
+            "otherContents" to SEEN,
+        ),
+        XSAnyAttribute.serializer() to mapOf(
+            "notQName" to SEEN,
+            "notNamespace" to SEEN,
+            "processContents" to SEEN,
+        ),
+        XSComplexContent.XSExtension.serializer() to mapOf(
+            "id" to SEEN,
+            "asserts" to SEEN,
+            "openContents" to SEEN,
+        ),
+        XSGroupRef.serializer() to mapOf(
+            "id" to SEEN,
+            "annotations" to SEEN,
+        ),
+        XSAll.serializer() to mapOf(
+            "groups" to SEEN,
+            "anys" to SEEN,
+        ),
+        XSLocalElement.serializer() to mapOf(
+            // nillable particlesIc06
+            "targetNamespace" to SEEN,
+            "alternatives" to SEEN,
+        ),
+        XSLocalComplexType.Serializer to mapOf(
+            "asserts" to SEEN,
+            "openContents" to SEEN,
+            "defaultAttributesApply" to SEEN,
+        ),
+        XSAny.serializer() to mapOf(
+            "processContents" to SEEN,
+            "notQName" to SEEN,
+            "notNamespace" to SEEN,
+        ),
+        XSAssert.serializer() to TAG_ABSENT,
+        XSLocalAttribute.serializer() to mapOf(
+            "inheritable" to SEEN,
+            "name" to ABSENT,
+            "targetNamespace" to SEEN,
+        ),
+        XSUnique.serializer() to mapOf(
+            "selector" to ABSENT,
+//            "fields" to ABSENT,
+            "ref" to SEEN,
+        ),
+        XSSelector.serializer() to mapOf("xpathDefaultNamespace" to SEEN),
+        XSField.serializer() to mapOf("xpathDefaultNamespace" to SEEN),
+        XSKey.serializer() to mapOf(
+            "selector" to ABSENT,
+//            "fields" to ABSENT,
+            "name" to ABSENT,
+            "ref" to SEEN,
+        ),
+        XSKeyref.serializer() to mapOf(
+            "selector" to ABSENT,
+//            "fields" to ABSENT,
+            "name" to ABSENT,
+            "ref" to SEEN,
+            "refer" to SEEN,
+        ),
+        XSOverride.serializer() to TAG_ABSENT,
+        XSDefaultOpenContent.serializer() to TAG_ABSENT,
+    )
+
     private suspend fun SequenceScope<DynamicNode>.testPropertyPresences(schemas: Sequence<XSSchema>) {
 
         val rootDescriptor = xml.xmlDescriptor(XSSchema.serializer()) as XmlRootDescriptor
 
         val schemaIterator = schemas.iterator()
 
-        val descriptors = extractDescriptors(rootDescriptor)
+        val descriptors = extractDescriptors(rootDescriptor).sortedBy { it.serialDescriptor.serialName.substringAfterLast('.') }
 
         val attributeViewer = AttributeViewer()
 
         for (desc in descriptors) {
-            val serialName = desc.serialDescriptor.serialName
-            dynamicContainer("Test presence/absence of ${serialName.substringAfterLast('.')} (${desc.tagName}) children") {
+            val serialName = desc.serialDescriptor.serialName.substringBeforeLast("?")
+            val expectations = expectedNonTested.filterKeys {
+                it.descriptor.serialName == serialName
+            }.values.singleOrNull() ?: emptyMap()
+
+            val ext = if (expectations.isEmpty()) "" else " (modified: ${expectations.size})"
+
+            dynamicContainer("Test presence/absence of ${serialName.substringAfterLast('.')} (${desc.tagName}) children$ext") {
                 val info = attributeViewer.structInfo(desc)
+
                 for (i in 0 until desc.elementsCount) {
                     val optional = desc.isElementOptional(i) || desc.getElementDescriptor(i).isNullable
                     val elementInfo = info[i]
-                    val displayName = desc.getElementDescriptor(i).tagName.localPart
-                    dynamicTest("#$i [$displayName] Check whether element is seen") {
-                        while (schemaIterator.hasNext() && (!elementInfo.seen || !(optional && elementInfo.hasBeenAbsent))) {
-                            val schema = schemaIterator.next()
-                            attributeViewer.encode(XSSchema.serializer(), schema, rootDescriptor)
+                    val displayName = desc.serialDescriptor.getElementName(i)
+                    val elementExpectation = expectations.getOrDefault(displayName, NONE)
+                    val extName = elementExpectation.name
+                    if (elementExpectation.testSeen) {
+                        dynamicTest("#$i [$displayName] Check whether element is seen ($extName)") {
+                            while (schemaIterator.hasNext() && (!elementInfo.seen || !(optional && elementInfo.hasBeenAbsent))) {
+                                val schema = schemaIterator.next()
+                                attributeViewer.encode(XSSchema.serializer(), schema, rootDescriptor)
+                            }
+                            assertTrue(elementInfo.seen)
                         }
-                        assertTrue(elementInfo.seen)
                     }
-                    if (optional) {
-                        dynamicTest("  - #$i [$displayName] Check whether this optional element has been omitted") {
+                    if (optional && elementExpectation.testAbsent) {
+                        dynamicTest("  - #$i [$displayName] Check whether this optional element has been omitted ($extName)") {
                             assertTrue(elementInfo.hasBeenAbsent)
                         }
                     }
