8da8747253d349cae53b8e2360cf8ed154451422
Simon Schirrmacher <simon.schirrmacher@mailbox.org>
2023-03-24 09:16:15 +0100

    add test for custom serialization and predefined entity refs bug

---
+++ serialization/src/commonTest/kotlin/nl/adaptivity/xml/serialization/ValueContainerWithCustomSerializerTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of xmlutil.
+ *
+ * This file is licenced to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You should have received a copy of the license with the source distribution.
+ * Alternatively, you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package nl.adaptivity.xml.serialization
+
+import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.*
+import nl.adaptivity.xmlutil.serialization.XmlSerialName
+import nl.adaptivity.xmlutil.serialization.XmlValue
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+class ValueContainerWithCustomSerializerTest : PlatformXmlTestBase<ValueContainerWithCustomSerializerTest.ValueContainer>(
+    ValueContainer(InnerValueContainer("<foo&bar>")),
+    ValueContainer.serializer()
+) {
+    override val expectedXML: String =
+        "<valueContainer><innerValueContainer>&lt;foo&amp;bar></innerValueContainer></valueContainer>"
+
+    @Serializable(with = CompatValueContainerSerializer::class)
+    @XmlSerialName("valueContainer")
+    data class ValueContainer(val inner: InnerValueContainer)
+
+    @Serializable
+    @XmlSerialName("innerValueContainer")
+    data class InnerValueContainer(@XmlValue(true) val content: String)
+
+    @Test
+    @Ignore
+    override fun testSerializeXml() {
+        super.testSerializeXml()
+    }
+
+    @Test
+    @Ignore
+    override fun testGenericSerializeXml() {
+        super.testGenericSerializeXml()
+    }
+
+    @Test
+    @Ignore
+    override fun testDeserializeXml() {
+        super.testDeserializeXml()
+    }
+
+    @Test
+    @Ignore
+    override fun testGenericDeserializeXml() {
+        super.testGenericDeserializeXml()
+    }
+}
