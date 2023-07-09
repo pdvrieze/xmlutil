30b7bc0de8c5c2c3c7dc7214e50d5a2b62cb84c6
Paul de Vrieze <paul.devrieze@gmail.com>
2023-07-09 20:09:27 +0100

    Allow . in namechars.

--- xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
+++ xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
@@ -62,7 +62,7 @@ class TestXSTestSuite {
             val suite = xml.decodeFromReader<TSTestSuite>(xmlReader)
             val subNodes = suite.testSetRefs
 //                .filter { false || it.href.contains("msMeta") }
-                .filter { false || (it.href.contains("sunMeta/suntest") /*&& it.href.contains("CType")*/) }
+                .filter { false || (it.href.contains("sunMeta/") /*&& it.href.contains("CType")*/) }
                 .map { setRef ->
 
                     val setBaseUrl: URI = javaClass.getResource("/xsts/${setRef.href}").toURI()
@@ -74,7 +74,7 @@ class TestXSTestSuite {
 
                     buildDynamicContainer("Test set $tsName") {
                         for (group in testSet.testGroups) {
-                            if (true || group.name.contains("xsd003a")) {
+                            if (true || group.name.contains("st_final00102m1")) {
                                 dynamicContainer("Group ${group.name}") {
                                     addSchemaTests(setBaseUrl, group)
                                 }
