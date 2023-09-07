8366a501df09ac53470360ada2b5d6a611940a96
Paul de Vrieze <paul.devrieze@gmail.com>
2023-09-07 22:24:21 +0100

    Implement the cC, iI (namechar and namestartchar) character classes.

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/regex/XRegex.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/regex/XRegex.kt
@@ -22,49 +22,6 @@ package io.github.pdvrieze.formats.xmlschema.regex
 
 import io.github.pdvrieze.formats.xmlschema.regex.impl.*
 
-private fun Iterable<XRRegexOption>.toInt(): Int = this.fold(0, { value, option -> value or option.value })
-
-private fun fromInt(value: Int): Set<XRRegexOption> =
-        XRRegexOption.entries.filterTo(mutableSetOf<XRRegexOption>()) { value and it.mask == it.value  }
-
-/**
- * Provides enumeration values to use to set regular expression options.
- */
-public enum class XRRegexOption(internal val value: Int, internal val mask: Int = value) {
-    // common
-
-    /** Enables case-insensitive matching. Case comparison is Unicode-aware. */
-    IGNORE_CASE(XRPattern.CASE_INSENSITIVE),
-
-    /**
-     * Enables multiline mode.
-     *
-     * In multiline mode the expressions `^` and `$` match just after or just before,
-     * respectively, a line terminator or the end of the input sequence.
-     */
-    MULTILINE(XRPattern.MULTILINE),
-
-    /**
-     * Enables literal parsing of the pattern.
-     *
-     * Metacharacters or escape sequences in the input sequence will be given no special meaning.
-     */
-    LITERAL(XRPattern.LITERAL),
-
-    /** Enables Unix lines mode. In this mode, only the `'\n'` is recognized as a line terminator. */
-    UNIX_LINES(XRPattern.UNIX_LINES),
-
-    /** Permits whitespace and comments in pattern. */
-    COMMENTS(XRPattern.COMMENTS),
-
-    /** Enables the mode, when the expression `.` matches any character, including a line terminator. */
-    DOT_MATCHES_ALL(XRPattern.DOTALL),
-
-    /** Enables equivalence by canonical decomposition. */
-    CANON_EQ(XRPattern.CANON_EQ)
-}
-
-
 /**
  * Represents the results from a single capturing group within a [MatchResult] of [Regex].
  *
@@ -97,15 +54,14 @@ public operator fun MatchGroupCollection.get(name: String): MatchGroup? {
  *
  * Note that in the future, the behavior of regular expression matching and replacement functions can be altered to match JVM implementation behavior where differences exist.
  */
-@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS") // Counterpart for @Suppress("ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS")
-public class XRRegex internal constructor(internal val nativePattern: XRPattern) {
+public class XRegex internal constructor(internal val nativePattern: XPattern) {
 
     internal enum class Mode {
         FIND, MATCH
     }
 
     /** Creates a regular expression from the specified [pattern] string and the default options.  */
-    constructor(pattern: String): this(XRPattern(pattern))
+    constructor(pattern: String): this(XPattern(pattern))
 
     /** The pattern string of this regular expression. */
     val pattern: String
@@ -113,16 +69,13 @@ public class XRRegex internal constructor(internal val nativePattern: XRPattern)
 
     private val startNode = nativePattern.startNode
 
-    /** The set of options that were used to create this regular expression.  */
-    val options: Set<XRRegexOption> = fromInt(nativePattern.flags)
-
     companion object {
 
         /**
          * Returns a regular expression pattern string that matches the specified [literal] string literally.
          * No characters of that string will have special meaning when searching for an occurrence of the regular expression.
          */
-        fun escape(literal: String): String = XRPattern.quote(literal)
+        fun escape(literal: String): String = XPattern.quote(literal)
 
         /**
          * Returns a literal replacement expression for the specified [literal] string.
@@ -230,151 +183,7 @@ public class XRRegex internal constructor(internal val nativePattern: XRPattern)
     }
 
     /**
-     * Replaces all occurrences of this regular expression in the specified [input] string with specified [replacement] expression.
-     *
-     * The replacement string may contain references to the captured groups during a match. Occurrences of `${name}` or `$index`
-     * in the replacement string will be substituted with the subsequences corresponding to the captured groups with the specified name or index.
-     * In case of `$index`, the first digit after '$' is always treated as a part of group reference. Subsequent digits are incorporated
-     * into `index` only if they would form a valid group reference. Only the digits '0'..'9' are considered as potential components
-     * of the group reference. Note that indexes of captured groups start from 1, and the group with index 0 is the whole match.
-     * In case of `${name}`, the `name` can consist of latin letters 'a'..'z' and 'A'..'Z', or digits '0'..'9'. The first character must be
-     * a letter.
-     *
-     * Backslash character '\' can be used to include the succeeding character as a literal in the replacement string, e.g, `\$` or `\\`.
-     * [Regex.escapeReplacement] can be used if [replacement] have to be treated as a literal string.
-     *
-     * @param input the char sequence to find matches of this regular expression in
-     * @param replacement the expression to replace found matches with
-     * @return the result of replacing each occurrence of this regular expression in [input] with the result of evaluating the [replacement] expression
-     * @throws RuntimeException if [replacement] expression is malformed, or capturing group with specified `name` or `index` does not exist
-     */
-    fun replace(input: CharSequence, replacement: String): String
-            = replace(input) { match -> substituteGroupRefs(match, replacement) }
-
-    /**
-     * Replaces all occurrences of this regular expression in the specified [input] string with the result of
-     * the given function [transform] that takes [MatchResult] and returns a string to be used as a
-     * replacement for that match.
-     */
-    fun replace(input: CharSequence, transform: (XMatchResult) -> CharSequence): String {
-        var match: XMatchResult? = find(input) ?: return input.toString()
-
-        var lastStart = 0
-        val length = input.length
-        val sb = StringBuilder(length)
-        do {
-            val foundMatch = match!!
-            sb.append(input, lastStart, foundMatch.range.start)
-            sb.append(transform(foundMatch))
-            lastStart = foundMatch.range.endInclusive + 1
-            match = foundMatch.next()
-        } while (lastStart < length && match != null)
-
-        if (lastStart < length) {
-            sb.append(input, lastStart, length)
-        }
-
-        return sb.toString()
-    }
-
-    /**
-     * Replaces the first occurrence of this regular expression in the specified [input] string with specified [replacement] expression.
-     *
-     * The replacement string may contain references to the captured groups during a match. Occurrences of `${name}` or `$index`
-     * in the replacement string will be substituted with the subsequences corresponding to the captured groups with the specified name or index.
-     * In case of `$index`, the first digit after '$' is always treated as a part of group reference. Subsequent digits are incorporated
-     * into `index` only if they would form a valid group reference. Only the digits '0'..'9' are considered as potential components
-     * of the group reference. Note that indexes of captured groups start from 1, and the group with index 0 is the whole match.
-     * In case of `${name}`, the `name` can consist of latin letters 'a'..'z' and 'A'..'Z', or digits '0'..'9'. The first character must be
-     * a letter.
-     *
-     * Backslash character '\' can be used to include the succeeding character as a literal in the replacement string, e.g, `\$` or `\\`.
-     * [Regex.escapeReplacement] can be used if [replacement] have to be treated as a literal string.
-     *
-     * @param input the char sequence to find a match of this regular expression in
-     * @param replacement the expression to replace the found match with
-     * @return the result of replacing the first occurrence of this regular expression in [input] with the result of evaluating the [replacement] expression
-     * @throws RuntimeException if [replacement] expression is malformed, or capturing group with specified `name` or `index` does not exist
-     */
-    fun replaceFirst(input: CharSequence, replacement: String): String {
-        val match = find(input) ?: return input.toString()
-        val length = input.length
-        val result = StringBuilder(length)
-        result.append(input, 0, match.range.start)
-        result.append(substituteGroupRefs(match, replacement))
-        if (match.range.endInclusive + 1 < length) {
-            result.append(input, match.range.endInclusive + 1, length)
-        }
-        return result.toString()
-    }
-
-    /**
-     * Splits the [input] CharSequence to a list of strings around matches of this regular expression.
-     *
-     * @param limit Non-negative value specifying the maximum number of substrings the string can be split to.
-     * Zero by default means no limit is set.
-     */
-    @Suppress("ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS")
-    fun split(input: CharSequence, limit: Int = 0): List<String> {
-        require(limit >= 0) { "Limit must be non-negative, but was $limit" }
-
-        var match: XMatchResult? = find(input)
-
-        if (match == null || limit == 1) return listOf(input.toString())
-
-        val result = ArrayList<String>(if (limit > 0) limit.coerceAtMost(10) else 10)
-        var lastStart = 0
-        val lastSplit = limit - 1 // negative if there's no limit
-
-        do {
-            result.add(input.substring(lastStart, match!!.range.start))
-            lastStart = match.range.endInclusive + 1
-            if (lastSplit >= 0 && result.size == lastSplit) break
-            match = match.next()
-        } while (match != null)
-
-        result.add(input.substring(lastStart, input.length))
-
-        return result
-    }
-
-    /**
-     * Splits the [input] CharSequence to a sequence of strings around matches of this regular expression.
-     *
-     * @param limit Non-negative value specifying the maximum number of substrings the string can be split to.
-     * Zero by default means no limit is set.
-     * @sample samples.text.Regexps.splitToSequence
-     */
-    @Suppress("ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS")
-    public fun splitToSequence(input: CharSequence, limit: Int = 0): Sequence<String> {
-        require(limit >= 0) { "Limit must be non-negative, but was $limit" }
-
-        return sequence {
-            var match = find(input)
-            if (match == null || limit == 1) {
-                yield(input.toString())
-                return@sequence
-            }
-
-            var nextStart = 0
-            var splitCount = 0
-
-            do {
-                val foundMatch = match!!
-                yield(input.substring(nextStart, foundMatch.range.first))
-                nextStart = foundMatch.range.endInclusive + 1
-                match = foundMatch.next()
-            } while (++splitCount != limit - 1 && match != null)
-
-            yield(input.substring(nextStart, input.length))
-        }
-    }
-
-    /**
      * Returns the string representation of this regular expression, namely the [pattern] of this regular expression.
-     *
-     * Note that another regular expression constructed from the same pattern string may have different [options]
-     * and may match strings differently.
      */
     override fun toString(): String = nativePattern.toString()
 }
