bd818b945f643e072031f3f5c8a2a31c25b4c889
Paul de Vrieze <paul.devrieze@gmail.com>
2023-09-20 21:44:25 +0100

    Fix checks for attribute groups (and redefines of them) -- this does not yet cover attribute wildcards.

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedGlobalAttributeGroup.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedGlobalAttributeGroup.kt
@@ -46,10 +46,37 @@ class ResolvedGlobalAttributeGroup(
 
     init {
         if (schema is CollatedSchema.RedefineWrapper) {
-            val baseAttrs = schema.nestedAttributeGroup(mdlQName).getAttributeUses().mapTo(HashSet()) { it.mdlAttributeDeclaration.mdlQName }
-            val attrUses = getAttributeUses().mapTo(HashSet()) { it.mdlAttributeDeclaration.mdlQName }
-            require(baseAttrs.all { it in attrUses } || attrUses.all { it in baseAttrs}) {
-                "Redefining attribute groups must be super or subsets of their bases.\n !(${attrUses.joinToString()}).containsAll(${baseAttrs.joinToString()})"
+            val selfRefCount = attributeGroups.count { it.resolvedGroup.mdlQName == mdlQName }
+
+            val baseGroup = schema.nestedAttributeGroup(mdlQName)
+            val baseAttrs = baseGroup.getAttributeUses().associateByTo(mutableMapOf()) { it.mdlAttributeDeclaration.mdlQName }
+            val attrUses = getAttributeUses()/*.mapTo(HashSet()) { it.mdlAttributeDeclaration.mdlQName }*/
+
+            when(selfRefCount) {
+                0 -> {
+                    for (dAttr in attrUses) {
+                        val dName = dAttr.mdlAttributeDeclaration.mdlQName
+                        val bAttr = requireNotNull(baseAttrs.remove(dName)) { "Attribute group redefines without self reference may only restrict" }
+                        require(dAttr.isValidRestrictionOf(bAttr)) {
+                            "3.4.6.3 - ${dAttr} doesn't restrict base attribute validly"
+                        }
+                    }
+                    for(b in baseAttrs.values) {
+                        require(! b.mdlRequired) {
+                            "Redefinition of attribute group must be a valid restriction, as such required attributes (${b.mdlAttributeDeclaration.mdlQName}) must remain present. "
+                        }
+                    }
+                    if (baseAttrs.values.any { it is ResolvedProhibitedAttribute }) {
+
+                    }
+                }
+
+                1 -> /*require(baseAttrs.all { it in attrUses }) {
+                    "Redefining attribute groups must be super or subsets of their bases.\n !(${attrUses.joinToString()}).containsAll(${baseAttrs.joinToString()})"
+                }*/
+                {} // automatically extends
+
+                else -> throw IllegalArgumentException("Multiple self-reference in attribute group redefine: $mdlQName")
             }
         }
     }
@@ -61,10 +88,18 @@ class ResolvedGlobalAttributeGroup(
         groups.add(this)
         while (groups.isNotEmpty()) {
             val group = groups.removeFirst()
+            val seenAttrNames = mutableSetOf<QName>()
             for (a in group.attributes) {
+                require(seenAttrNames.add(a.mdlAttributeDeclaration.mdlQName)) {
+                    "Duplicate attribute name ${a.mdlAttributeDeclaration.mdlQName} in attribute group $mdlQName"
+                }
                 uses.put(a.mdlAttributeDeclaration.mdlQName, a)
             }
+            val seenGroupNames = mutableSetOf<QName>()
             for (g in group.attributeGroups) {
+                require(seenGroupNames.add(g.ref)) {
+                    "Duplicate nested attribute group name ${g.ref} in attribute group $mdlQName"
+                }
                 val resolvedGroup = g.resolvedGroup
                 if (seenGroups.add(resolvedGroup)) {
                     groups.add(resolvedGroup)
