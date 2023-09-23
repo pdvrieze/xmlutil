ef8f84a47baa9bee07b7bd2019374cdf8282e2fd
Paul de Vrieze <paul.devrieze@gmail.com>
2023-09-23 20:55:43 +0100

    Make attributes unique transitively in a group, not only detect duplicates in a single group non-transitively.

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedGlobalAttributeGroup.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedGlobalAttributeGroup.kt
@@ -85,10 +85,10 @@ class ResolvedGlobalAttributeGroup(
         val uses = mutableMapOf<QName, IResolvedAttributeUse>()
         val seenGroups = mutableSetOf(this)
         val groups = ArrayDeque<ResolvedGlobalAttributeGroup>()
+        val seenAttrNames = mutableSetOf<QName>()
         groups.add(this)
         while (groups.isNotEmpty()) {
             val group = groups.removeFirst()
-            val seenAttrNames = mutableSetOf<QName>()
             for (a in group.attributes) {
                 require(seenAttrNames.add(a.mdlQName)) {
                     "Duplicate attribute name ${a.mdlQName} in attribute group $mdlQName"
