48d18fbe4eaeafbbe9b9309ee84430aac71f43f9
Paul de Vrieze <paul.devrieze@gmail.com>
2023-12-11 13:54:08 +0000

    Use assertion dsl for all path tests (simplifies them a lot).

--- xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xpath/test/pathAssertions.kt
+++ xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xpath/test/pathAssertions.kt
@@ -22,14 +22,15 @@ package io.github.pdvrieze.formats.xpath.test
 
 import io.github.pdvrieze.formats.xpath.XPathExpression
 import io.github.pdvrieze.formats.xpath.impl.*
-import io.github.pdvrieze.formats.xpath.impl.BinaryExpr
-import io.github.pdvrieze.formats.xpath.impl.XPathInternal
 import nl.adaptivity.xmlutil.QName
 import nl.adaptivity.xmlutil.SimpleNamespaceContext
 import nl.adaptivity.xmlutil.XmlEvent
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertTrue
+
+@DslMarker
+annotation class PathTestDsl
 
 fun testPath(path: String, vararg namespaces: Pair<String, String>, test: TestContext.() -> Unit) {
     val nsContext = SimpleNamespaceContext(namespaces.map { (p, ns) -> XmlEvent.NamespaceImpl(p, ns) })
@@ -38,6 +39,7 @@ fun testPath(path: String, vararg namespaces: Pair<String, String>, test: TestCo
 }
 
 @OptIn(XPathInternal::class)
+@PathTestDsl
 interface TestContext {
     val expr: Expr
     fun <T: Expr> nestedContext(expr: T):ExprContext<T>
@@ -49,7 +51,8 @@ interface ExprContext<T: Expr>: TestContext {
 }
 
 @OptIn(XPathInternal::class)
-internal class PathContext(private val path: LocationPath) {
+@PathTestDsl
+internal class PathContext(val path: LocationPath) {
     private fun getStep(): Step {
         assertTrue(stepCount < path.steps.size, "Expected at least $stepCount steps, but found only ${path.steps.size}")
         return path.steps[stepCount++]
@@ -58,22 +61,94 @@ internal class PathContext(private val path: LocationPath) {
     fun assertStepSelf() {
         val step = getStep()
         assertEquals(Axis.SELF, step.axis)
-        assertEquals(NodeTest.AnyNameTest, step.test)
+        assertEquals(NodeTest.NodeTypeTest(NodeType.NODE), step.test)
         assertEquals(0, step.predicates.size)
     }
 
+    fun assertStepDescendant() {
+        assertStep(Axis.DESCENDANT_OR_SELF, NodeType.NODE)
+    }
+
     fun assertStep(axis: Axis, qName: QName) {
+        assertStep(axis, qName) {}
+    }
+
+    fun assertStep(axis: Axis, nodeType: NodeType) {
+        assertStep(axis, nodeType) {}
+    }
+
+    fun assertStep(s: String) = assertStep(Axis.CHILD, QName(s))
+
+    inline fun assertStep(s: String, test: StepContext.() -> Unit) {
+        assertStep(QName(s), test)
+    }
+
+    inline fun assertStep(name: QName, test: StepContext.() -> Unit) {
+        assertStep(Axis.CHILD, name, test)
+    }
+
+    inline fun assertStep(axis: Axis, qName: QName, test: StepContext.() -> Unit) {
         val step = getStep()
         assertEquals(axis, step.axis)
         val t = assertIs<NodeTest.QNameTest>(step.test)
         assertEquals(qName, t.qName)
+        val ctx = StepContext(step)
+        ctx.test()
+        assertEquals(ctx.predicateCount, step.predicates.size, "Expected ${ctx.predicateCount} predicates but found ${step.predicates.size}")
     }
+
+    inline fun assertStep(axis: Axis, nodeType: NodeType, test: StepContext.() -> Unit) {
+        val step = getStep()
+        assertEquals(axis, step.axis)
+        val t = assertIs<NodeTest.NodeTypeTest>(step.test)
+        assertEquals(nodeType, t.type)
+
+        val ctx = StepContext(step)
+        ctx.test()
+        assertEquals(ctx.predicateCount, step.predicates.size, "Expected ${ctx.predicateCount} predicates but found ${step.predicates.size}")
+    }
+
+    inline fun <reified T: NodeTest> assertStep(axis: Axis, test: StepContext.(T) -> Unit) {
+        val step = getStep()
+        assertEquals(axis, step.axis)
+        val ctx = StepContext(step)
+        ctx.test(assertIs<T>(step.test))
+        assertEquals(ctx.predicateCount, step.predicates.size, "Expected ${ctx.predicateCount} predicates but found ${step.predicates.size}")
+    }
+
+    inline fun <reified T: NodeTest> assertStep(test: StepContext.(T) -> Unit) {
+        assertStep<T>(Axis.CHILD, test)
+    }
+
+    fun assertStep(axis: Axis, localName: String) {
+        assertStep(axis, QName(localName))
+    }
+
 
     internal var stepCount = 0
         private set
 }
 
 @OptIn(XPathInternal::class)
+internal class StepContext(val step: Step) {
+    private fun getPredicate(): ExprContext<Expr> {
+        assertTrue(predicateCount < step.predicates.size, "Expected at least $predicateCount steps, but found only ${step.predicates.size}")
+        return ExprContextImpl(step.predicates[predicateCount++])
+    }
+
+    inline fun assertPredicate(test: TestContext.() -> Unit) {
+        val pred = getPredicate()
+        pred.test()
+    }
+
+    var predicateCount: Int = 0
+        private set
+
+
+}
+
+@OptIn(XPathInternal::class)
+@PathTestDsl
 internal inline fun ExprContext<LocationPath>.assertPath(rooted: Boolean? = null, test: PathContext.() -> Unit) {
     if (rooted!=null) assertEquals(rooted, expr.rooted)
     val ctx = PathContext(expr).apply(test)
@@ -102,6 +177,30 @@ internal inline fun <reified T1 : Expr, reified T2 : Expr> TestContext.assertBin
 
     nestedContext(assertIs<T1>(bin.left)).apply(left)
     nestedContext(assertIs<T2>(bin.right)).apply(right)
+}
+
+@OptIn(XPathInternal::class)
+internal fun TestContext.assertPath(test: PathContext.() -> Unit) {
+    val path = assertIs<LocationPath>(expr)
+    val c = PathContext(path)
+    c.test()
+    assertEquals(c.stepCount, path.steps.size)
+}
+
+@OptIn(XPathInternal::class)
+internal fun TestContext.assertNumber(value: Long) {
+    val n = assertIs<NumberLiteral>(expr)
+    assertEquals(value, n.value)
+}
+
+internal fun TestContext.assertFunctionCall(name: String) {
+    assertFunctionCall(QName(name))
+}
+@OptIn(XPathInternal::class)
+internal fun TestContext.assertFunctionCall(name: QName) {
+    val f = assertIs<FunctionCall>(expr)
+    assertEquals(name, f.name)
+    assertEquals(0, f.args.size)
 }
 
 @OptIn(XPathInternal::class)
