cdf1aad73d9f65b1f936870686c2292c4aac45a6
Paul de Vrieze <paul.devrieze@gmail.com>
2023-12-12 20:34:59 +0000

    Group references in all must be minOccurs=1 and maxOccurs=1

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/IResolvedAll.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/IResolvedAll.kt
@@ -20,6 +20,7 @@
 
 package io.github.pdvrieze.formats.xmlschema.resolved
 
+import io.github.pdvrieze.formats.xmlschema.datatypes.primitiveInstances.VNonNegativeInteger
 import io.github.pdvrieze.formats.xmlschema.resolved.ResolvedModelGroup.Compositor
 import io.github.pdvrieze.formats.xmlschema.resolved.checking.CheckHelper
 import io.github.pdvrieze.formats.xmlschema.types.AllNNIRange
@@ -42,6 +43,13 @@ interface IResolvedAll : ResolvedModelGroup {
                 }
                 if (particle !is ResolvedProhibitedElement) {
                     check(particle.mdlTerm is ResolvedElement) { "For version 1.0 all groups may only contain elements, not any's" }
+                }
+            } else { // v1.1
+                if (particle is ResolvedGroupRef) { // groupRefs are not permitted in 1.0
+                    require(particle.mdlMinOccurs == VNonNegativeInteger.ONE) { "Particle references in all must have 1 as minOccurs" }
+                    require(particle.mdlMaxOccurs == VAllNNI.ONE) { "Particle references in all must have 1 as maxOccurs" }
+
+                    check(particle.mdlTerm is IResolvedAll) { "Group references in all must be referencing all particles" }
                 }
             }
         }
