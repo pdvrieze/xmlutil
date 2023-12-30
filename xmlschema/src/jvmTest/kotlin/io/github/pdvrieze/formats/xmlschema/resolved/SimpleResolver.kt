9a45aeaad7f5dc06dd192235cd888cda0dfb3609
Paul de Vrieze <paul.devrieze@gmail.com>
2023-12-30 22:52:11 +0000

    Use byte order mark to detect encoding.

--- xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/SimpleResolver.kt
+++ xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/SimpleResolver.kt
@@ -25,11 +25,12 @@ import io.github.pdvrieze.formats.xmlschema.datatypes.primitiveInstances.toAnyUr
 import io.github.pdvrieze.formats.xmlschema.datatypes.serialization.XSSchema
 import nl.adaptivity.xmlutil.EventType
 import nl.adaptivity.xmlutil.XmlReader
-import nl.adaptivity.xmlutil.core.impl.newReader
 import nl.adaptivity.xmlutil.serialization.XML
 import nl.adaptivity.xmlutil.xmlStreaming
+import java.io.BufferedInputStream
 import java.io.FileNotFoundException
 import java.io.InputStream
+import java.io.InputStreamReader
 import java.net.URI
 import java.net.URL
 
@@ -129,8 +130,27 @@ private inline fun <R> URL.withXmlReader(body: (XmlReader) -> R): R {
 }
 
 private inline fun <R> InputStream.withXmlReader(body: (XmlReader) -> R): R {
-    return use { inStream ->
-        val reader = xmlStreaming.newReader(inStream, "UTF-8")
+    val stream = when {
+        markSupported() -> this
+        else -> BufferedInputStream(this)
+    }
+    return stream.use { inStream ->
+        inStream.mark(4)
+        val c = inStream.read()
+        val charset: String = when {
+            c == 0xef && read() == 0xbb && read() == 0xbf -> "UTF-8"
+
+            c == 0xfe && read() == 0xff -> "UTF-16BE"
+
+            c == 0xff && read() == 0xfe -> "UTF-16LE"
+
+            else -> {
+                inStream.reset()
+                "UTF-8"
+            }
+        }
+
+        val reader = xmlStreaming.newGenericReader(InputStreamReader(inStream, charset))
         val r = reader.use(body)
         if (reader.eventType != EventType.END_DOCUMENT) {
             var e: EventType
