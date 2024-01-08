40fe79125077797ede206218f762e56b39159d53
Paul de Vrieze <paul.devrieze@gmail.com>
2024-01-08 21:39:52 +0000

    Try to fix the check on "restriction"

--- xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedLocalAttribute.kt
+++ xmlschema/src/commonMain/kotlin/io/github/pdvrieze/formats/xmlschema/resolved/ResolvedLocalAttribute.kt
@@ -56,7 +56,7 @@ class ResolvedLocalAttribute private constructor(
             check(parent is ResolvedComplexType) { "3.2.3(6.3.1) - Attribute with non-matching namespace must have complex type ancestor"}
             check(parent.mdlDerivationMethod == VDerivationControl.RESTRICTION)
             val contentType = parent.mdlContentType
-            check(contentType !is ResolvedComplexType.EmptyContentType) { "Restricting an empty content type is not possible" }
+            check(contentType !is ResolvedComplexType.ResolvedSimpleContentType) { "Restricting an empty content type is not possible ($rawPart)" }
             check(parent.mdlBaseTypeDefinition != AnyType) { "3.2.3(6.3.2) - Restriction isn't anytype" }
         }
 
