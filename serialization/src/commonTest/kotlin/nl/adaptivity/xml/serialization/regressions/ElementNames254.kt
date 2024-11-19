1d380a2b3c531f413b363fccc68651b1ee4603e7
Paul de Vrieze <paul.devrieze@gmail.com>
2024-11-19 20:15:05 +0000

    Add a test for issue #254. Unfortunately this is a limitation of the underlying library (a pull request has been submitted: kotlin/kotlinx.serialization#2862) for a fix. In the meantime either disabling the cache or using `@XmlSerialName` instead of `@SerialName` will fix it.

---
+++ serialization/src/commonTest/kotlin/nl/adaptivity/xml/serialization/regressions/ElementNames254.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2024.
+ *
+ * This file is part of xmlutil.
+ *
+ * This file is licenced to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You should have received a copy of the license with the source distribution.
+ * Alternatively, you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package nl.adaptivity.xml.serialization.regressions
+
+import io.github.pdvrieze.xmlutil.testutil.assertXmlEquals
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import nl.adaptivity.xmlutil.QName
+import nl.adaptivity.xmlutil.serialization.FormatCache
+import nl.adaptivity.xmlutil.serialization.OutputKind
+import nl.adaptivity.xmlutil.serialization.XML
+import nl.adaptivity.xmlutil.serialization.XmlElement
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/** Test for #254 where somehow the name is incorrect */
+class ElementNames254 {
+
+    lateinit var xml: XML
+
+    @BeforeTest
+    fun setup() {
+        xml= XML {
+            recommended {
+                // TODO Remove this check once a fix exists in the library.
+                formatCache = FormatCache.Dummy
+            }
+            indent = 4
+        }
+    }
+
+    @Serializable
+    @SerialName("element")
+    data class Element(
+        @XmlElement(true)
+        val a: String
+    )
+
+    @Serializable
+    @SerialName("element")
+    data class OtherElement(
+        @XmlElement(true)
+        val b: String
+    )
+
+    @Serializable
+    data class Parent(
+        val element: Element,
+    )
+
+    @Serializable
+    data class OtherParent(
+        val element: OtherElement,
+    )
+
+    @Serializable
+    data class Root(
+        val parent: Parent,
+        val otherParent: OtherParent,
+    )
+
+    @Test
+    fun testDescriptor() {
+        val desc = xml.xmlDescriptor(Root.serializer()).getElementDescriptor(0)
+        assertEquals(2, desc.elementsCount)
+        val pDesc = desc.getElementDescriptor(0)
+        assertEquals(QName("Parent"), pDesc.tagName)
+        val elem= pDesc.getElementDescriptor(0)
+        assertEquals(QName("element"), elem.tagName)
+        val aAttr = elem.getElementDescriptor(0)
+        assertEquals(QName("a"), aAttr.tagName)
+
+        val opDesc = desc.getElementDescriptor(1)
+        assertEquals(QName("OtherParent"), opDesc.tagName)
+        val otherElem = opDesc.getElementDescriptor(0)
+        assertEquals(QName("element"), otherElem.tagName)
+
+        val bAttr = otherElem.getElementDescriptor(0)
+        assertEquals(QName("b"), bAttr.tagName)
+        assertEquals(OutputKind.Element, bAttr.effectiveOutputKind)
+    }
+
+    @Test
+    fun testSerialize() {
+        val root = Root(
+            parent = Parent(Element("element")),
+            otherParent = OtherParent(OtherElement("element")),
+        )
+        val serialized = xml.encodeToString(root)
+        assertXmlEquals(EXPECTED, serialized)
+    }
+
+    @Test
+    fun testDeserialize() {
+        val root = Root(
+            parent = Parent(Element("element")),
+            otherParent = OtherParent(OtherElement("element")),
+        )
+        val deserialized: Root = XML.decodeFromString(EXPECTED)
+
+        assertEquals(root, deserialized)
+    }
+
+    val EXPECTED = """|<?xml version="1.1"?>
+        |<Root>
+        |  <Parent><element><a>element</a></element></Parent>
+        |  <OtherParent><element><b>element</b></element></OtherParent>
+        |</Root>
+    """.trimMargin()
+}
