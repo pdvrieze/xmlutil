8b5ee721acfd87b7a8762f361eda8b30adae1efe
Paul de Vrieze <paul.devrieze@gmail.com>
2025-06-25 12:24:55 +0100

    Explicitly disable generating JPMS modules for modules where it is not needed, and add a module-info.java for core-io

--- schemaTests/build.gradle.kts
+++ schemaTests/build.gradle.kts
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2025.
+ *
+ * This file is part of xmlutil.
+ *
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 import net.devrieze.gradle.ext.addNativeTargets
 
 /*
@@ -33,6 +53,10 @@ base {
 }
 
 val autoModuleName = "net.devrieze.xmlutil.schema.tests"
+
+config {
+    generateJavaModules = false // test code, not published
+}
 
 kotlin {
     js {
