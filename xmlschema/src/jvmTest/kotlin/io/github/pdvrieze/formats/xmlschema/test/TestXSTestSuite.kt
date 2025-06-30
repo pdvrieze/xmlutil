b7adbdc065ed81de2d0f5296327bcf7d530d2884
Paul de Vrieze <paul.devrieze@gmail.com>
2025-06-30 12:18:42 +0100

    Make all tests pass (ignoring dom reliant tests in the nodejs context)

--- xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
+++ xmlschema/src/jvmTest/kotlin/io/github/pdvrieze/formats/xmlschema/test/TestXSTestSuite.kt
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2023.
+ * Copyright (c) 2023-2025.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package io.github.pdvrieze.formats.xmlschema.test
@@ -42,12 +42,14 @@ import org.w3.xml.xmschematestsuite.override.CompactOverride
 import org.w3.xml.xmschematestsuite.override.OTSSuite
 import java.net.URI
 import java.net.URL
-import kotlin.datetime.Instant
 import kotlin.experimental.ExperimentalTypeInference
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
 import kotlin.test.assertTrue
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
+@OptIn(ExperimentalTime::class)
 class TestXSTestSuite {
 
     var xml: XML = XML {
