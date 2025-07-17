8e4997693b3e7cdeb20fbe3b0df903cccb6b54b4
Paul de Vrieze <paul.devrieze@gmail.com>
2025-07-17 12:55:45 +0100

    Update to the new maven centtral compatibility api endpoints.

--- project-plugins/src/main/kotlin/net/devrieze/gradle/ext/publishing.kt
+++ project-plugins/src/main/kotlin/net/devrieze/gradle/ext/publishing.kt
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2024.
+ * Copyright (c) 2024-2025.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package net.devrieze.gradle.ext
@@ -53,11 +53,11 @@ fun Project.doPublish(
                 val repositoryId = project.properties["xmlutil.repositoryId"] as String?
                 url = when {
                     "SNAPSHOT" in version.toString().uppercase(Locale.getDefault()) ->
-                        uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                        uri("https://central.sonatype.com/repository/maven-snapshots/")
                     repositoryId != null ->
-                        uri("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId/")
+                        uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deployByRepositoryId/$repositoryId/")
                     else ->
-                        uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                        uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
                 }
                 credentials {
                     username = project.findProperty("ossrh.username") as String?
