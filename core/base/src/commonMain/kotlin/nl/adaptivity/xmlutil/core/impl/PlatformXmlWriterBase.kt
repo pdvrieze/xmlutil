9d66e121ff37f8efaad90105a2753d4db289d229
Paul de Vrieze <paul.devrieze@gmail.com>
2025-10-28 17:38:19 +0000

    Escape any indent string (but note that non-whitespace content should break things).

--- core/base/src/commonMain/kotlin/nl/adaptivity/xmlutil/core/impl/PlatformXmlWriterBase.kt
+++ core/base/src/commonMain/kotlin/nl/adaptivity/xmlutil/core/impl/PlatformXmlWriterBase.kt
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2024.
+ * Copyright (c) 2024-2025.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package nl.adaptivity.xmlutil.core.impl
@@ -32,7 +32,16 @@ public abstract class PlatformXmlWriterBase(indentSequence: Iterable<XmlEvent.Te
             _indentString = value.joinToString("") { ev ->
                 when (ev.eventType) {
                     EventType.COMMENT -> "<!--${ev.text}-->"
-                    else -> ev.text
+                    else -> buildString {
+                        for (c in ev.text) {
+                            when (c) {
+                                '<' -> append("&lt;")
+                                '>' -> append("&gt;")
+                                '&' -> append("&amp;")
+                                else -> append(c)
+                            }
+                        }
+                    }
                 }
             }
 
