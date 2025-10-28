dfac4ab6803f71361d311ac3e5051a1d56a59349
Paul de Vrieze <paul.devrieze@gmail.com>
2025-10-28 17:29:00 +0000

    Various simple updates to the dom interface to deprecate unused code.

--- core/base/src/commonDomMain/kotlin/nl/adaptivity/xmlutil/core/impl/idom/INodeList.kt
+++ core/base/src/commonDomMain/kotlin/nl/adaptivity/xmlutil/core/impl/idom/INodeList.kt
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2024.
+ * Copyright (c) 2024-2025.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 @file:Suppress("DEPRECATION")
@@ -28,7 +28,8 @@ import nl.adaptivity.xmlutil.dom2.Node as Node2
 import nl.adaptivity.xmlutil.dom2.NodeList as NodeList2
 
 public interface INodeList : NodeList1, NodeList2, Collection<Node2> {
-    @Deprecated("Use size", ReplaceWith("size"))
+    // Deprecation retained to ensure getLength is still implemented. I
+    @Deprecated("Use size", ReplaceWith("size"), DeprecationLevel.HIDDEN)
     public override fun getLength(): Int = size
 
     override fun item(index: Int): INode?
@@ -49,5 +50,5 @@ public interface INodeList : NodeList1, NodeList2, Collection<Node2> {
         return elements.all { contains(it) } // inefficient
     }
 
-    public override fun isEmpty(): Boolean = getLength() == 0
+    public override fun isEmpty(): Boolean = size == 0
 }
