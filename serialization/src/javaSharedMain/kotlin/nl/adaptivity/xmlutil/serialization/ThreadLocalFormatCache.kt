01aaae7c28ea3632143001ff5a2514e9be617879
Paul de Vrieze <paul.devrieze@gmail.com>
2025-11-12 20:07:15 +0000

    Add default constructor argument to initialize the Thread

--- serialization/src/javaSharedMain/kotlin/nl/adaptivity/xmlutil/serialization/ThreadLocalFormatCache.kt
+++ serialization/src/javaSharedMain/kotlin/nl/adaptivity/xmlutil/serialization/ThreadLocalFormatCache.kt
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2024.
+ * Copyright (c) 2024-2025.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package nl.adaptivity.xmlutil.serialization
@@ -28,7 +28,7 @@ import nl.adaptivity.xmlutil.serialization.structure.*
 import java.lang.ref.SoftReference
 import java.util.function.Supplier
 
-public class ThreadLocalFormatCache(private val baseCacheFactory: Supplier<FormatCache>) : FormatCache() {
+public class ThreadLocalFormatCache(private val baseCacheFactory: Supplier<FormatCache> = Supplier { DefaultFormatCache() }) : FormatCache() {
 
     private var threadLocal = ThreadLocal.withInitial(Weaken(baseCacheFactory))
 
